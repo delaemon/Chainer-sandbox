95fe974eada26846057645f3cb435b594dfbf32f
delaemon <takeshi.music.5@gmail.com>
2017-06-12 06:13:45 +0900

    First commit

---
+++ chainer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from sklearn.datasets import fetch_mldata
+from sklearn.cross_validation import train_test_split
+from sklearn.svm import LinearSVC as Classifier
+from sklearn.metrics import confusion_matrix
+import numpy as np
+
+mnist = fetch_mldata("MNIST original", data_home=".")
+data = np.asarray(mnist.data, np.float32)
+data_train, data_test, label_train, label_test = train_test_split(data, mnist.target, test_size=0.2)
+
+classifier = Classifier()
+classifier.fit(data_train, label_train)
+
+result = classifier.predict(data_test)
+
+cmat = confusion_matrix(label_test, result)
+print(cmat)
